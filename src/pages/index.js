fce623a151b5cdd1da4e5161e57cf2f37b1add12
Kenny Nguyen <kenny@prontotv.no>
2021-09-17 09:55:10 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Ad from "../components/Ad"
+
+import "./styles.css"
+
+export default function IndexPage() {
+  return (
+    <>
+      <Ad />
+    </>
+  )
+}
